100b885de9d388fb3dd837fe1dc9293d24c22980
Benny Nguyen <benny.nguyenx@gmail.com>
2018-04-11 09:13:05 -0700

    Finish testing all required dialogue in log

--- app/src/main/java/edu/washington/bennyn/activityspy/MainActivity.kt
+++ app/src/main/java/edu/washington/bennyn/activityspy/MainActivity.kt
@@ -6,27 +6,42 @@ import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
-    val tag: String = "MainActivity"
+    val TAG: String = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.i(tag, "onCreate event fired")
+        Log.i(TAG, "onCreate event fired")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.i(tag, "onStart event fired")
+        Log.i(TAG, "onStart event fired")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart event fired")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.i(tag, "onResume event fired")
+        Log.i(TAG, "onResume event fired")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.i(tag, "onPause event fired")
+        Log.i(TAG, "onPause event fired")
+    }
+
+    override fun onStop() {
+        super.onPause()
+        Log.i(TAG, "onStop event fired")
+    }
+
+    override fun onDestroy() {
+        super.onPause()
+        Log.e(TAG, "We're going down, Captain!")
     }
 }
